22bdd88bb06cfaaf1772b48bfbcdac51edf71a63
cpanel10x <vutn0911@gmail.com>
2022-03-21 22:05:45 +0700

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -37,6 +37,7 @@ class Config(object):
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    TAOBAO_PROXY = os.environ.get("TAOBAO_PROXY", "")
     # https://t.me/hevcbay/951
     OUO_IO_API_KEY = ""
     # maximum message length in Telegram
